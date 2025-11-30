192d20e78249406c1e7a89ac55beefb713203988
Toan Nguyen <hgiasac@gmail.com>
2025-11-30 22:17:58 +0700

    add metrics

--- server/config.go
+++ server/config.go
@@ -3,12 +3,15 @@ package main
 import (
 	"errors"
 	"fmt"
+	"slices"
 
 	"github.com/caarlos0/env/v11"
 	"github.com/hasura/gotel"
+	"github.com/relychan/gohttpc"
 	"github.com/relychan/gohttps"
 	"github.com/relychan/goutils"
 	"github.com/relychan/rely-auth/auth"
+	"github.com/relychan/rely-auth/auth/authmode"
 )
 
 var errConfigPathRequired = errors.New("config path is required")
@@ -47,6 +50,19 @@ func InitAuthManager(
 	authConfig, err := goutils.ReadJSONOrYAMLFile[auth.RelyAuthConfig](environment.ConfigPath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to load auth config: %w", err)
+	}
+
+	if slices.ContainsFunc(authConfig.Definitions, func(def auth.RelyAuthDefinition) bool {
+		mode := def.GetMode()
+		// enable http client metrics if JWT or Auth webhook mode exists
+		return mode == authmode.AuthModeJWT || mode == authmode.AuthModeWebhook
+	}) {
+		httpClientMetrics, err := gohttpc.NewHTTPClientMetrics(exporters.Meter, false)
+		if err != nil {
+			return nil, fmt.Errorf("failed to setup http client meitrcs: %w", err)
+		}
+
+		gohttpc.SetHTTPClientMetrics(httpClientMetrics)
 	}
 
 	manager, err := auth.NewRelyAuthManager(
